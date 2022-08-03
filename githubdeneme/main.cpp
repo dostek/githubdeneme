50e08534f9648bbacd05718adcc96979e159ec93
İsmail KARAGOZ <ikaragoz@gmail.com>
2022-08-03 17:52:26 +0300

    Initial Commit

---
+++ githubdeneme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  githubdeneme
+//
+//  Created by Ismail KARAGOZ on 3.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
